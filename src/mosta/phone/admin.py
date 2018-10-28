ed224e81c0d637602a40fc05ad69b20c5ad9f72d
Nico Schwanebeck <gitlab@nischwan.de>
2018-10-28 15:16:06 +0100

    register phone models to admin site

--- src/mosta/phone/admin.py
+++ src/mosta/phone/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Phone, Sim, CallHistory, ChargingHistory, BalanceHistory, SMS, PowerSocket
+
+admin.site.register(Phone)
+admin.site.register(Sim)
+admin.site.register(CallHistory)
+admin.site.register(ChargingHistory)
+admin.site.register(BalanceHistory)
+admin.site.register(SMS)
+admin.site.register(PowerSocket)
